a02a10a80bb273d88be6c0321bdebe46e35144a0
franhendrawan <franhendrawan0@gmail.com>
2026-06-10 01:47:27 +0700

    finished tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  celcius = (temp - 32) * 5/9;
+  rounded = Math.ceil(celcius * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  fahr = (temp * 9/5 + 32);
+  rounded = Math.ceil(fahr * 10) / 10;
+  return rounded
 };
 
 // Do not edit below this line
