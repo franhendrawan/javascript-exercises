81ab2088ca19a9063d65fe0670985e32c1eee8cf
franhendrawan <franhendrawan0@gmail.com>
2026-06-09 14:36:50 +0700

    finish repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
+    else {
+        let text = '';
+        while (num > 0) {
+            text += string;
+            num--;
+        }
+        return text;
+    }
 };
 
 // Do not edit below this line
