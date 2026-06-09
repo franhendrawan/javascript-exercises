edd04b59652f48bb9bbbcbc27719289a2f65f662
franhendrawan <franhendrawan0@gmail.com>
2026-06-09 15:02:21 +0700

    finish reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const originalText = Array.from(string)
+    const reversedText = originalText.toReversed();
+    const result = reversedText.join('');
+    return result.toString()
+    
 };
 
 // Do not edit below this line
