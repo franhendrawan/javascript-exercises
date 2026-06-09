9b7fe6adf693ad4334515f8000501c248acf994a
franhendrawan <franhendrawan0@gmail.com>
2026-06-10 01:04:06 +0700

    finished sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
+    let result = [];
+    if (first < 0 || last < 0) {
+        return 'ERROR';
+    }
+    if (!Number.isInteger(first) || !Number.isInteger(last)){
+        return 'ERROR';
+    }
+    if (first > last) {
+        let temp = last;
+        last = first;
+        first = temp;
+    }
 
+    for (let i = first; i <= last; i++){
+        result.push(i)
+    }
+
+    const sumOfAll = result.reduce((total, currentItem) => {
+        return total + currentItem;
+    })
+    return sumOfAll;
 };
 
 // Do not edit below this line
